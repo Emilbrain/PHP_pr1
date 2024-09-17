23df8c4d4484aa4e8873c639d2fe2ee5b2d47fa8
Emilbrain <74027280+Emilbrain@users.noreply.github.com>
2024-09-17 16:50:32 +0300

    section

--- task2.php
+++ task2.php
@@ -1,19 +1,22 @@
 <link rel="stylesheet" href="assets/style/style.css">
+<div class="section">
 
-<?php
-$arr = [
-    "Спортивный" => "Сидоров",
-    "Художественный" => "Емелина",
-    "Музыкальный" => "Иванова",
-    "Литературный" => "Петров",
-    "Биологический" => "Антонова"
-];
+    <?php
+    $arr = [
+        "Спортивный" => "Сидоров",
+        "Художественный" => "Емелина",
+        "Музыкальный" => "Иванова",
+        "Литературный" => "Петров",
+        "Биологический" => "Антонова"
+    ];
 
-asort($arr);
+    asort($arr);
 
-foreach ($arr as $arr => $surname) {
-    echo "<span><h3>$arr</h3> - $surname</span>";
-?>
-    <br>
-<?
-}
+    foreach ($arr as $arr => $surname) {
+        echo "<span><h3>$arr</h3> - $surname</span>";
+    ?>
+        <br>
+    <?
+    }
+    ?>
+</div>
